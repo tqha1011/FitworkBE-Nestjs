1f681d77199f6034285894359a2a91d4ed00de98
Tran Quang Ha <tranquangha2006@gmail.com>
2026-09-12 11:39:24 +0700

    feat: update type role in jwt payload

--- src/shared/common/jwt.payload.interface.ts
+++ src/shared/common/jwt.payload.interface.ts
@@ -1,10 +1,11 @@
 import { UUID } from 'crypto';
 import { Request } from 'express';
+import { CommonUserRole } from '../domain/enum';
 
 export interface JwtPayload {
   email: string;
   sub: UUID;
-  role: string; // consider using enum type for better type safety
+  role: CommonUserRole; // consider using enum type for better type safety
 }
 
 export interface RequestWithUser extends Request {
